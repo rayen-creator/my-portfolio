cc8e132e9373d01a3f30f5fb21ae71b4443209b1
Rayen Oueslati <rayenoueslati60@gmail.com>
2023-06-22 19:10:41 +0100

    🔥

--- src/app/canvas/moon.js
+++ src/app/canvas/moon.js
@@ -1,3 +1,4 @@
+"use client";
 import React, { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
