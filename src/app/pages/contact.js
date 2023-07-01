10a1400b22e9dbf4b99220fe68661f7da918de43
Rayen Oueslati <rayenoueslati60@gmail.com>
2023-07-01 12:59:11 +0100

    🔥

--- src/app/pages/contact.js
+++ src/app/pages/contact.js
@@ -18,7 +18,7 @@ export default function Contact() {
           <Title title={"Contact"} />
         </h3>
       </Reveal>
-      <div className="grid grid-cols-1  md:grid-cols-2 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-4">
         <div className="col-span-1">
           <div class="grid grid-cols-1  md:grid-cols-2 gap-3">
             <div class="md:col-span-2 ">
